dd4e03fb5ae61aeb82cd76123d466956ff9267d5
sophtrn <s66tran@uwaterloo.ca>
2022-07-26 12:01:49 -0400

    Update main.js

--- main/Samples/main.js
+++ main/Samples/main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas");
-canvas.width = 50%;
-canvas.height = 50%;
+canvas.width = window.innerWidth - 60;
+canvas.height = 400;
 
 let context= canvas.getContext("2d")
 context.fillStyle = "white";
